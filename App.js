e373120f4aa7b5818ef810152595e8eada008817
Pedro Bergamini <54374893+bergaminipedro@users.noreply.github.com>
2022-08-02 02:23:59 -0300

    Tela Inicio

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { AppRoutes } from './src/Routes';
+import TelaInicial from './src/pages/Inicio/inicio';
+
+
 export default function App() {
   return (
-      <AppRoutes/>
+      <TelaInicial/>
   );
 }
